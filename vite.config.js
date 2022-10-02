a320e0dcef1773c9545ddf7a4bef8fb44bc56fb6
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-03 01:40:16 +0300

    vite build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
+
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [vue()],
+});
